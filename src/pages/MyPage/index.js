12fc71f1739c1b9bf5e3643052c46b948884fad2
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-05-26 16:29:36 +0900

    docs: my page에 user_id 가져옴

--- src/pages/MyPage/index.js
+++ src/pages/MyPage/index.js
@@ -3,11 +3,21 @@ import MyPageItemes from "../../components/MyPage/MyPageItems";
 import MyPageClubItems from "../../components/MyPage/MyPageClubItems";
 import styled from "styled-components";
 import manager_user from "../../mockdata/mypage/manager_user.json"
+import {getCookie, setCookie} from "../../auth/cookie";
+import async from "async";
+import axios from "axios";
+import {closePopup} from "../../redux/loginPopup";
 
 function MyPage() {
+    const user_id = getCookie('user_id')
+    console.log('user_id:',user_id);
+    console.log('api 받아올 주소:::',`https://dingdong7.pythonanywhere.com/mypage/${user_id}`);
+
     const user_data = [manager_user];
+
     return (
-        <>  <Wrap>
+        <>
+            <Wrap>
                <PageHeader/>
                 <ItemBox>
                     <MyPageItemes data={user_data}/>
