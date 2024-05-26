f9df0c630e62950c9982c1af728f44016175d0bc
KimJeongJoo <kjjeaus@hufs.ac.kr>
2024-05-26 16:29:54 +0900

    test: user_id로 데이터 받아오기 TEST

--- src/pages/TestPage/index.js
+++ src/pages/TestPage/index.js
@@ -1,15 +1,13 @@
-import TestComponent from "../../components/TestPage/TestComponent";
+
 import allclubs from '../../mockdata/mainpage/allclubs.json'
 import TestMockData from "../../components/TestPage/TestMockData";
 import manager_user from "../../mockdata/mypage/manager_user.json"
-import TestMyPageMockData from "../../components/TestPage/TestMyPageMockData";
-import TestRedux from "../../components/TestPage/TestRedux";
-import TestHandleRedux from "../../components/TestPage/TestHandleRedux";
 
-import TestMyPage from "../../components/TestPage/TestMypage";
-import TestMyPageDetail from "../../components/TestPage/TestMypageDetail";
-import Loading from "../../components/LoadingSpinner/Loading";
-import {Link, useLocation, useNavigate, useParams} from "react-router-dom";
+import {useParams} from "react-router-dom";
+import axios from "axios";
+import {getCookie} from "../../auth/cookie";
+import {useEffect, useState} from "react";
+
 
 function TestPage() {
     // json.에서 불러온 데이터 새로운 변수에 저장
@@ -21,11 +19,27 @@ function TestPage() {
     //동적 라우팅
     const params = useParams();
 
+    const user_id = getCookie('user_id');
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await axios.get(
+                    `https://dingdong7.pythonanywhere.com/mypage/${user_id}`,
+                    { withCredentials: true }
+                );
+                console.log('response:', response);
+
+            } catch (error) {
+                console.log('error:', error);
+
+            }
+        }
+    }, [user_id]);
 
 
     return (
         <>
-            {/*<Loading/>*/}
             <TestMockData data={data} params={params}/>
 
         </>
